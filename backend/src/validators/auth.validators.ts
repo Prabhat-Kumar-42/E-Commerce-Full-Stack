1a5ea13869cac3ffe353bdcdc053cf97b0c272ba
Prabhat <prabhatkumarverma42@gmail.com>
2025-09-08 06:34:50 +0530

    feat(validation): add signup and login validation schemas using zod

---
+++ backend/src/validators/auth.validators.ts
@@ -0,0 +1,13 @@
+
+// /src/validators/auth.validators.ts
+import { z } from "zod";
+
+export const signupSchema = z.object({
+  email: z.email("Invalid email address"),
+  password: z.string().min(6, "Password must be at least 6 characters"),
+});
+
+export const loginSchema = z.object({
+  email: z.email("Invalid email address"),
+  password: z.string().min(1, "Password is required"),
+});
